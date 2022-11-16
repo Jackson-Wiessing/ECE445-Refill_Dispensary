10da1ae8fa64286161582c5bb054c6a0e4d854d2
Lyla Zegelstein <lrz12351@gmail.com>
2022-11-16 12:59:18 -0600

    takes awhile but gets a semi-stable reading

--- loadCell.py
+++ loadCell.py
@@ -2,6 +2,52 @@ from hx711 import HX711
 from utime import sleep_us
 from time import sleep
 
+def remove_outliers():
+        # this function needs to take the average of stabilized values & then convert it to a weight in grams
+        print("entered remove outliers")
+        vals = [-10, -10, -10, -10, -10, -10, -10]
+        
+        # 1. Get 7 readings from stable values
+        print("getting readings")
+        for i in range(len(vals)):
+            print("reading in # ", i)
+            reading = -10
+            while reading < 0:
+                reading = scales.stable_value()
+            vals[i] = reading
+            
+        # 2. check for outliers
+        print("checking for outliers")
+        vals.sort()
+        print("vals: ", vals)
+        median = vals[3]
+        upper_fence = median + 1000
+        lower_fence = median - 1000
+
+
+        outlier_idx = []
+        for i in range(len(vals)):
+            if i > upper_fence or i < lower_fence:
+                outlier_idx.append(i)
+    
+        print("outliers are at indices: ", outlier_idx)
+        
+        for i in outlier_idx:
+            r = scales.stable_value()
+            while (r < lower_fence or r > upper_fence):
+                r = scales.stable_value()
+            vals[i] = r
+        
+        average = 0
+        for i in vals:
+            average += i
+        
+        return average / 7
+    
+def scale_values(val):
+    # TODO:
+    return val
+
 
 class Scales(HX711):
     def __init__(self, d_out, pd_sck):
@@ -31,13 +77,16 @@ class Scales(HX711):
         for prev in values:
             weights.append(sum([1 for current in values if abs(prev - current) / (prev / 100) <= deviation]))
         return sorted(zip(values, weights), key=lambda x: x[1]).pop()[0]
-
+    
+                    
 
 if __name__ == "__main__":
     scales = Scales(d_out=5, pd_sck=6)
     scales.tare()
     while True:
-        val = scales.stable_value()
-        print(val)
+        #val = scales.stable_value()
+       # print(val)
+       res = remove_outliers()
+       print(res)
        
     scales.power_off()
