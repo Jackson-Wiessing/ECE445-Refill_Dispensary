09dd512522c3f5ba2be567f10b7eb01c487a678d
Lyla Zegelstein <lrz12351@gmail.com>
2022-11-27 14:15:47 -0600

    Update screen.py

--- microPythonPlaying/screen.py
+++ microPythonPlaying/screen.py
@@ -1,23 +1,18 @@
 from machine import Pin, I2C
-
 from ssd1306 import SSD1306_I2C
-
 from oled import Write, GFX, SSD1306_I2C
-
 from oled.fonts import ubuntu_mono_15, ubuntu_mono_20
-
 import utime
 
+WIDTH = 128
 
-WIDTH =128
+HEIGHT = 64
 
-HEIGHT= 64
-
-i2c=I2C(0,sda=Pin(0),scl=Pin(1),freq=200000)
+i2c = I2C(1, scl = Pin(3), sda = Pin(2), freq = 200000)
 
 print("i2c initialization done")
 
-oled = SSD1306_I2C(WIDTH,HEIGHT,i2c)
+oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)
 
 print("post line 22")
 
@@ -27,8 +22,9 @@ write20 = Write(oled, ubuntu_mono_20)
 
 write20.text("OLED", 0, 0)
 
-write15.text("Display", 0, 10)
+write15.text("Display", 0, 30)
 
-oled.text("ElectroniClinic", 0, 20)
+oled.text("ElectroniClinic", 0, 50)
 
 oled.show()
+
